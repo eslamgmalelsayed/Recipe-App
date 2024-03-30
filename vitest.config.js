bd8dc41ededd2d4795c5e42e00fdf68a12f55d6d
islam <eslamgmal1@gmail.com>
2024-03-30 15:21:38 +0300

    gh-pages-sett

--- vitest.config.js
+++ vitest.config.js
@@ -5,6 +5,7 @@ import viteConfig from './vite.config'
 export default mergeConfig(
   viteConfig,
   defineConfig({
+    base: /Recipe-App/,
     test: {
       environment: 'jsdom',
       exclude: [...configDefaults.exclude, 'e2e/*'],
