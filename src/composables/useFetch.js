67c0c1bcb00032bf5c7e1ce145759a4a2114859c
islam <eslamgmal1@gmail.com>
2024-03-30 16:50:00 +0300

    remove unnecessary files

--- src/composables/useFetch.js
+++ src/composables/useFetch.js
@@ -7,7 +7,6 @@ export default function useFetch() {
   const error = ref(null)
 
   const fetchData = async url => {
-  //  url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=5277${Math.floor(Math.random() * 9)}`;
     try {
       loading.value = true
       const response = await axios.get(url+Math.floor(Math.random() * 9))
--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-// import ForbiddenView from '../views/forbiddenView.vue'
-
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    // {
-    //   path: '/:pathMatch(.*)',
-    //   name: 'forbidden',
-    //   component: ForbiddenView
-    // }
+  
   ]
 })
 
